ab9f2f7b6e1d7653b35b95dc829d2bf65adb45ab
PonzyPon <11908295+PonzyPon@users.noreply.github.com>
2020-01-25 10:36:45 +0900

    Create token

---
+++ token/token.go
@@ -0,0 +1,35 @@
+package token
+
+// TokenType は、字句解析器が出力するトークンの種類を表す
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// 識別子 + リテラル
+	IDENT = "IDENT" // add, foobar, x, y,...
+	INT   = "INT"   // 12345
+
+	// 演算子
+	ASSIGN = "="
+	PLUST  = "+"
+
+	// デリミタ
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// キーワード
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
