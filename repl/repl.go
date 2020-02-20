0298fffa5dec835cfe1ec48ba58a16f5665f2a51
PonzyPon <11908295+PonzyPon@users.noreply.github.com>
2020-02-20 23:43:27 +0900

    3.9 Create environment

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/PonzyPon/monkey/object"
+
 	"github.com/PonzyPon/monkey/parser"
 
 	"github.com/PonzyPon/monkey/evaluator"
@@ -28,6 +30,7 @@ const MONKEY_FACE = `            __,__
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -46,7 +49,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
